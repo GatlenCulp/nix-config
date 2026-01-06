0ba7f4bae028404ac15459b7c529dffec32214aa
GatlenCulp <GatlenCulp@gmail.com>
2026-01-05 23:26:15 -0500

    fix: duti defaults

--- home/duti/default.nix
+++ home/duti/default.nix
@@ -1,25 +1,39 @@
 { pkgs, lib, ... }:
+let
+  # Declarative file association mappings: bundleId -> [extensions]
+  associations = {
+    "com.adobe.Acrobat.Pro" = [ "pdf" ];
+    "com.microsoft.VSCode" = [
+      "json"
+      "html"
+      "css"
+      "py"
+      "nix"
+      "csv"
+    ];
+    "pl.maketheweb.cleanshotx" = [
+      "jpg"
+      "jpeg"
+      "png"
+    ];
+    "org.m0k.transmission" = [ "torrent" ];
+  };
+
+  # Generate duti commands from associations
+  mkDutiCommands =
+    associations:
+    lib.concatStringsSep "\n" (
+      lib.flatten (
+        lib.mapAttrsToList (
+          bundleId: exts: map (ext: "${pkgs.duti}/bin/duti -s ${bundleId} ${ext} all") exts
+        ) associations
+      )
+    );
+in
 {
   home.packages = [ pkgs.duti ];
 
   home.activation."configureDutiAppDefaults" = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    # Office
-    duti -s com.adobe.Acrobat.Pro pdf all
-
-    # Code
-    duti -s com.microsoft.VSCode json all
-    duti -s com.microsoft.VSCode html all
-    duti -s com.microsoft.VSCode css all
-    duti -s com.microsoft.VSCode py all
-    duti -s com.microsoft.VSCode nix all
-    duti -s com.microsoft.VSCode csv all
-
-    # Images
-    duti -s pl.maketheweb.cleanshotx jpg all
-    duti -s pl.maketheweb.cleanshotx jpeg all
-    duti -s pl.maketheweb.cleanshotx png all
-
-    # Misc
-    duti -s org.m0k.transmission torrent all
+    ${mkDutiCommands associations}
   '';
 }
